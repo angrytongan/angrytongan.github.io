9ccaa54c1926327467b351975ead8517f7440fa3
Dean Fogarty <angrytongan@gmail.com>
2019-06-25 20:25:43 +1000

    WIP

---
+++ js/app.js
@@ -0,0 +1,84 @@
+/*
+ * Based on samples from https://threejsfundamentals.org
+ */
+'use strict';
+
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    const fov = 75;
+    const aspect = 2;
+    const near = 0.1;
+    const far = 1000;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+    camera.position.set(4, 8, 6);
+
+    const controls = new THREE.OrbitControls(camera, canvas);
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+    const scene = new THREE.Scene();
+
+    {
+        const color = 0xffffff;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+    }
+
+    const radius = 4;
+    const geometry = new THREE.DodecahedronBufferGeometry(radius);
+
+    function makeInstance(geometry, color, x) {
+        const material = new THREE.MeshPhongMaterial({ color });
+        const object = new THREE.Mesh(geometry, material);
+
+        scene.add(object);
+        object.position.x = x;
+
+        return object;
+    }
+
+    const objects = [
+        makeInstance(geometry, 0x44aa88, 0),
+    ];
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render(time) {
+        time *= 0.001; // convert time to seconds
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        objects.forEach((obj, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            obj.rotation.x = rot * 0.25;
+            obj.rotation.y = rot * 0.15;
+        });
+
+        controls.update();
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
+}
+
+main();
