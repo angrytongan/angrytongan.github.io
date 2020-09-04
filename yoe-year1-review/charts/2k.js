ec7926b66e0ee9803b988c82ed7662b97cb1d0ca
Dean Fogarty <angrytongan@gmail.com>
2020-09-04 21:29:26 +1000

    Hack hack hack...

--- yoe-year1-review/charts/2k.js
+++ yoe-year1-review/charts/2k.js
@@ -11,69 +11,109 @@ const summary = () => {
     const tooltip = [];
 
     const data = [];
-    data.push({
-        date: '2018-10-18',
-        time: '7:27.6',
-        type: '2k Training',
-        link: 'https://log.concept2.com/profile/1063564/log/35115315',
-    }, {
-        date: '2018-06-10',
-        time: '6:51.8',
-        type: '2k Competition',
-        link: 'https://log.concept2.com/profile/1063564/log/33807825',
-    }, {
-        date: '2017-12-14',
-        time: '7:35.5',
-        type: '2k Training',
-        link: 'https://log.concept2.com/profile/1063564/log/31627410',
-    }, {
-        date: '2017-11-16',
-        time: '6:57.4',
-        type: '2k Attempt',
-        link: 'https://log.concept2.com/profile/1063564/log/31250250',
-    }, {
-        date: '2017-08-29',
-        time: '7:44.5',
-        type: '2k Training',
-        link: 'https://log.concept2.com/profile/1063564/log/30511391',
-    }, {
-        date: '2017-08-09',
-        time: '7:47.9',
-        type: '2k Training',
-        link: 'https://log.concept2.com/profile/1063564/log/30341046',
-    }, {
-        date: '2016-09-18',
-        time: '7:28.4',
-        type: '2k Training',
-        link: 'https://log.concept2.com/profile/1063564/log/27326493',
-    }, {
-        date: '2016-02-08',
-        time: '7:36.5',
-        type: '2k Training',
-        link: 'https://log.concept2.com/profile/1063564/log/26072228',
-    }, {
-        date: '2015-11-02',
-        time: '7:42.2',
-        type: '2k Training',
-        link: 'https://log.concept2.com/profile/1063564/log/22568174',
-    }, {
-        date: '2019-12-07',
-        time: '7:36.5',
-        type: 'YoE 3x2k/3:00r attempt 1',
-        link: '',
-    }, {
-        date: '2019-12-07',
-        time: '7:34.8',
-        type: 'YoE 3x2k/3:00r attempt 1',
-        link: '',
-    }, {
-        date: '2019-12-07',
-        time: '7:31.3',
-        type: 'YoE 3x2k/3:00r attempt 1',
-        link: '',
-    });
+    data.push(
+        {
+            date: '2019-07-13',
+            time: '7:50.6',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/38867588',
+        },
+        {
+            date: '2018-10-18',
+            time: '7:27.6',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/35115315',
+        },
+        {
+            date: '2018-06-10',
+            time: '6:51.8',
+            type: 'Competition',
+            link: 'https://log.concept2.com/profile/1063564/log/33807825',
+        },
+        {
+            date: '2018-01-04',
+            time: '7:56.9',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/31896011',
+        },
+        {
+            date: '2017-12-14',
+            time: '7:35.5',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/31627410',
+        },
+        {
+            date: '2017-11-16',
+            time: '6:57.4',
+            type: 'Attempt',
+            link: 'https://log.concept2.com/profile/1063564/log/31250250',
+        },
+        {
+            date: '2017-08-29',
+            time: '7:44.5',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/30511391',
+        },
+        {
+            date: '2017-08-09',
+            time: '7:47.9',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/30341046',
+        },
+        {
+            date: '2016-09-18',
+            time: '7:28.4',
+            type: 'Attempt',
+            link: 'https://log.concept2.com/profile/1063564/log/27326493',
+        },
+        {
+            date: '2017-07-17',
+            time: '7:46.1',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/26964018',
+        },
+        {
+            date: '2016-03-01',
+            time: '8:22.2',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/26072232',
+        },
+        {
+            date: '2016-02-08',
+            time: '7:36.5',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/26072228',
+        },
+        {
+            date: '2015-11-02',
+            time: '7:42.2',
+            type: 'Training',
+            link: 'https://log.concept2.com/profile/1063564/log/22568174',
+        },
+
+        /* YoE */
+        {
+            date: '2019-12-07',
+            time: '7:36.5',
+            type: 'YoE 3x2k/3:00r attempt 1',
+            link: '',
+        },
+        {
+            date: '2019-12-07',
+            time: '7:34.8',
+            type: 'YoE 3x2k/3:00r attempt 1',
+            link: '',
+        },
+        {
+            date: '2019-12-07',
+            time: '7:31.3',
+            type: 'YoE 3x2k/3:00r attempt 1',
+            link: '',
+        }
+    );
 
     legend.push({
+        type: 'scroll',
         top: '10%',
     });
 
