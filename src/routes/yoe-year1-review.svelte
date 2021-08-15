00039063dedd11c88232f1d9ae3956c842a9a7c5
Dean Fogarty <angrytongan@gmail.com>
2021-08-15 13:06:45 +1000

    Add redirect for yoe year 1 review.

---
+++ src/routes/yoe-year1-review.svelte
@@ -0,0 +1,8 @@
+<script lang="ts" context="module">
+    export async function load() {
+        return {
+            status: 301,
+            redirect: '/recreation/yoe-year1-review',
+        }
+    };
+</script>
