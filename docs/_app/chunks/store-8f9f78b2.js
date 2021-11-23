09d03a6e39080ad71b9d8ab9a7cb3cf5807e75c8
Dean Fogarty <angrytongan@gmail.com>
2021-11-23 19:48:23 +1000

    Fix up breadcrumb example.

---
+++ docs/_app/chunks/store-8f9f78b2.js
@@ -0,0 +1,1 @@
+import{C as e}from"./vendor-5fde3abd.js";const t=((t,s,r=[],o=[])=>{{const e=localStorage.getItem(t);e&&(s=o.reduce(((e,t)=>t(e)),e))}const{subscribe:c,set:a}=e(s);return{subscribe:c,set:e=>(localStorage.setItem(t,r.reduce(((e,t)=>t(e)),e)),a(e))}})("theme","light");export{t};
