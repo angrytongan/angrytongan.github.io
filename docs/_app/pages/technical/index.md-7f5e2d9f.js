a7941f1cee6f93f3f0edae311bf9d76706432b2b
Dean Fogarty <angrytongan@gmail.com>
2022-09-30 14:27:21 +1000

    Regenerate.

---
+++ docs/_app/pages/technical/index.md-7f5e2d9f.js
@@ -0,0 +1,15 @@
+import{S as F,i as G,s as J,r,x as n,a as M,u as o,v as s,y as i,n as t,d as N,w as f,f as O,I as e,C}from"../../chunks/index-077af1fa.js";function Q(B){let a,S,l,h,x,y,u,A,b,m,k,q,_,c,p,I,K,d,L,T;return{c(){a=r("ul"),S=r("li"),l=r("p"),h=r("a"),x=n(`Transforming data to and from storage areas using Svelte custom
+stores`),y=n(" - A lightning talk for "),u=r("a"),A=n(`Svelte Summit
+2021`),b=n(` where a simple mechanism is presented for
+transforming data to and from storage areas using a custom Svelte store.
+`),m=r("a"),k=n("Source and transcript"),q=M(),_=r("li"),c=r("p"),p=r("a"),I=n("Svelte examples"),K=n(" - "),d=r("a"),L=n("Svelte"),T=n(` is a user
+interface toolkit for web applications that greatly improves the developer
+experience. Here you\u2019ll find a few articles, as well as quite a bit of code,
+demonstrating how to use Svelte and SvelteKit.`),this.h()},l(w){a=o(w,"UL",{});var v=s(a);S=o(v,"LI",{});var H=s(S);l=o(H,"P",{});var g=s(l);h=o(g,"A",{href:!0,rel:!0});var P=s(h);x=i(P,`Transforming data to and from storage areas using Svelte custom
+stores`),P.forEach(t),y=i(g," - A lightning talk for "),u=o(g,"A",{href:!0,rel:!0});var D=s(u);A=i(D,`Svelte Summit
+2021`),D.forEach(t),b=i(g,` where a simple mechanism is presented for
+transforming data to and from storage areas using a custom Svelte store.
+`),m=o(g,"A",{href:!0,rel:!0});var U=s(m);k=i(U,"Source and transcript"),U.forEach(t),g.forEach(t),H.forEach(t),q=N(v),_=o(v,"LI",{});var Z=s(_);c=o(Z,"P",{});var E=s(c);p=o(E,"A",{href:!0});var j=s(p);I=i(j,"Svelte examples"),j.forEach(t),K=i(E," - "),d=o(E,"A",{href:!0,rel:!0});var z=s(d);L=i(z,"Svelte"),z.forEach(t),T=i(E,` is a user
+interface toolkit for web applications that greatly improves the developer
+experience. Here you\u2019ll find a few articles, as well as quite a bit of code,
+demonstrating how to use Svelte and SvelteKit.`),E.forEach(t),Z.forEach(t),v.forEach(t),this.h()},h(){f(h,"href","https://youtu.be/1Df-9EKvZr0?t=6195"),f(h,"rel","nofollow"),f(u,"href","https://sveltesummit.com"),f(u,"rel","nofollow"),f(m,"href","https://github.com/angrytongan/svelte-summit-2021"),f(m,"rel","nofollow"),f(p,"href","/technical/svelte"),f(d,"href","http://svelte.dev"),f(d,"rel","nofollow")},m(w,v){O(w,a,v),e(a,S),e(S,l),e(l,h),e(h,x),e(l,y),e(l,u),e(u,A),e(l,b),e(l,m),e(m,k),e(a,q),e(a,_),e(_,c),e(c,p),e(p,I),e(c,K),e(c,d),e(d,L),e(c,T)},p:C,i:C,o:C,d(w){w&&t(a)}}}const V=!0;class W extends F{constructor(a){super(),G(this,a,null,Q,J,{})}}export{W as default,V as prerender};
