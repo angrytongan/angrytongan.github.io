5598667f226c3ca03d74dd226e9230d19303f29a
Dean Fogarty <angrytongan@gmail.com>
2023-01-28 20:19:55 +1000

    Update.

--- docs/_app/chunks/store-a7f9cedd.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./vendor-3a28259b.js";const t=((t,s,r=[],o=[])=>{{const e=localStorage.getItem(t);e&&(s=o.reduce(((e,t)=>t(e)),e))}const{subscribe:c,set:a}=e(s);return{subscribe:c,set:e=>(localStorage.setItem(t,r.reduce(((e,t)=>t(e)),e)),a(e))}})("theme","light");export{t};
